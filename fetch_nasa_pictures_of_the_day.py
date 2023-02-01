45b3eb6fdda6e688cbed6e5873a1a857dc751899
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-01 22:47:29 +0400

    file upload bug fixed

--- fetch_nasa_pictures_of_the_day.py
+++ fetch_nasa_pictures_of_the_day.py
@@ -7,15 +7,12 @@ from dotenv import load_dotenv
 
 def fetch_nasa_pictures_of_the_day():
     url = "https://api.nasa.gov/planetary/apod"
-    payload = {'api_key': os.environ['NASA_API_KEY']}
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    nasa_pictures_of_the_day = response.json()
+    nasa_pictures_of_the_day = get_url(url).json()
 
-    for picture_number, picture in enumerate(nasa_pictures_of_the_day):
+    for picture_number in enumerate(nasa_pictures_of_the_day):
         name_picture_template = """nasa_apod{number}.jpg""".format(number=picture_number)
         picture_for_telegram = os.path.join("APOD", name_picture_template)
-        download_images(picture, picture_for_telegram)
+        download_images(nasa_pictures_of_the_day['url'], picture_for_telegram)
 
 
 def get_url(url):
