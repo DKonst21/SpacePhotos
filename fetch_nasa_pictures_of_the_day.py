2933f31751d69ca7a3fb4ca5dc15089eddeddf24
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-05 00:10:23 +0400

    isolated environment variable

--- fetch_nasa_pictures_of_the_day.py
+++ fetch_nasa_pictures_of_the_day.py
@@ -5,26 +5,21 @@ from download_images import download_images
 from dotenv import load_dotenv
 
 
-def fetch_nasa_pictures_of_the_day():
-    url = "https://api.nasa.gov/planetary/apod"
-    nasa_pictures_of_the_day = get_url(url).json()
+def fetch_nasa_pictures_of_the_day(response):
 
-    for picture_number in enumerate(nasa_pictures_of_the_day):
-        name_picture_template = """nasa_apod{number}.jpg""".format(number=picture_number)
-        picture_for_telegram = os.path.join("APOD", name_picture_template)
-        download_images(nasa_pictures_of_the_day['url'], picture_for_telegram)
-
-
-def get_url(url):
-    payload = {'api_key': os.environ['NASA_API_KEY']}
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    return response
+    nasa_pictures_of_the_day = response.json()
+    name_picture_template = """nasa_apod{number}.jpg""".format(number=str(nasa_pictures_of_the_day['date']))
+    picture_for_telegram = os.path.join("APOD", name_picture_template)
+    download_images(nasa_pictures_of_the_day['url'], picture_for_telegram)
 
 
 def main():
     load_dotenv()
-    fetch_nasa_pictures_of_the_day()
+    url = "https://api.nasa.gov/planetary/apod"
+    payload = {'api_key': os.environ['NASA_API_KEY']}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    fetch_nasa_pictures_of_the_day(response)
 
 
 if __name__ == '__main__':
