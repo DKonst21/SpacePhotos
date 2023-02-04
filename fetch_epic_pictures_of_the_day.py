d8c7e735966efed2295f61e2c076ad3088c49b35
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-05 00:09:22 +0400

    function added get_url()

--- fetch_epic_pictures_of_the_day.py
+++ fetch_epic_pictures_of_the_day.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime
 
 from datetime import datetime
 from download_images import download_images
@@ -7,7 +8,7 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 
-def fetch_epic_pictures_of_the_day():
+def fetch_epic_pictures_of_the_day(api_key_foto):
     url_nasa = "https://api.nasa.gov/EPIC/api/natural/images"
     epic_pictures = get_url(url_nasa).json()
 
@@ -17,24 +18,28 @@ def fetch_epic_pictures_of_the_day():
     for index_number, index in enumerate(epic_pictures):
         massiv_image = index['image']
         massiv_date = (datetime.fromisoformat(index['date']))
-        url_foto_earth_split[6] = str(massiv_date.year)
-        url_foto_earth_split[7] = str(massiv_date.month)
-        url_foto_earth_split[8] = str(massiv_date.day)
+        url_foto_earth_split[6] = massiv_date.strftime("%Y")
+        url_foto_earth_split[7] = massiv_date.strftime("%m")
+        url_foto_earth_split[8] = massiv_date.strftime("%d")
         disassembled_url = urlparse(url_foto_earth)
         path = disassembled_url.path.split('/')
         path[4:7] = url_foto_earth_split[6:9]
 
         url_foto_earth_split[10] = massiv_image
-        api_key_foto = {'api_key': os.environ['NASA_API_KEY']}
         url_actual_foto = f"{'/'.join(url_foto_earth_split)}.{url_foto_earth_split[9]}"
         responce_url_actual_foto = requests.get(url_actual_foto, params=api_key_foto)
         responce_url_actual_foto.raise_for_status()
-        name_epic_picture_template = os.path.join("images", """{name}.png""".format(name=massiv_image))
+        name_epic_picture_template = os.path.join("epic", """{name}.png""".format(name=massiv_image))
         download_images(responce_url_actual_foto.url, name_epic_picture_template)
 
 
 def get_url(url):
-    payload = {"api_key": 'DEMO_KEY'}
+    epic_api_token = 'DEMO_KEY'
+    input("Введите токен:")
+    if input() != epic_api_token:
+        payload = {"api_key": input().replace("\r", "")}
+    else:
+        payload = {"api_key": 'DEMO_KEY'}
     response = requests.get(url, params=payload)
     response.raise_for_status()
     return response
@@ -42,7 +47,8 @@ def get_url(url):
 
 def main():
     load_dotenv()
-    fetch_epic_pictures_of_the_day()
+    api_key_foto = {'api_key': os.environ['NASA_API_KEY']}
+    fetch_epic_pictures_of_the_day(api_key_foto)
 
 
 if __name__ == '__main__':
