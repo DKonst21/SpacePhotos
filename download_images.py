4f286403adc34b4235057cccb82f77ea49f05110
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-07 23:41:47 +0400

    added function create_directory

--- download_images.py
+++ download_images.py
@@ -1,10 +1,16 @@
 import requests
+import os
+
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 def download_images(url, file_path):
 
     response = requests.get(url)
     response.raise_for_status()
-    
+
     with open(file_path, 'wb') as file:
         return file.write(response.content)
