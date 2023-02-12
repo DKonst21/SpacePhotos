47a344ba3f194b2978d1243b5f19a91b972444ed
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-13 00:16:19 +0400

    redundant function removed, code optimized

--- fetch_epic_pictures_of_the_day.py
+++ fetch_epic_pictures_of_the_day.py
@@ -4,16 +4,12 @@ import datetime
 
 from datetime import datetime
 from download_images import download_images
-from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 
 def fetch_epic_pictures_of_the_day(api_key_foto):
     nasa_url = "https://api.nasa.gov/EPIC/api/natural/images"
-    epic_pictures = get_url(nasa_url).json()
-
-    url_foto_earth = "https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png"
-    url_foto_earth_split = get_url(url_foto_earth).url.split('/')
+    epic_pictures = get_url(nasa_url, api_key_foto).json()
 
     for index_number, index in enumerate(epic_pictures):
         massiv_image = index['image']
@@ -21,32 +17,17 @@ def fetch_epic_pictures_of_the_day(api_key_foto):
         year = massiv_date.strftime("%Y")
         month = massiv_date.strftime("%m")
         day = massiv_date.strftime("%d")
-        url_foto_earth_split[6:9] = [year, month, day]
-        disassembled_url = urlparse(url_foto_earth)
-        path = disassembled_url.path.split('/')
-        path[4:7] = [year, month, day]
-
-        url_foto_earth_split[10] = massiv_image
-        url_actual_foto = f"{'/'.join(url_foto_earth_split)}.{url_foto_earth_split[9]}"
-        responce_url_actual_foto = requests.get(url_actual_foto, params=api_key_foto)
-        responce_url_actual_foto.raise_for_status()
-        name_epic_picture_template = os.path.join("epic", """{name}.png""".format(name=massiv_image))
-        download_images(responce_url_actual_foto.url, name_epic_picture_template)
+        url_actual_foto = f"https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{massiv_image}.png"
+        response_url_actual_foto = requests.get(url_actual_foto, params=api_key_foto)
+        response_url_actual_foto.raise_for_status()
+        name_epic_picture_template = os.path.join("epic", "{name}.png".format(name=massiv_image))
+        download_images(response_url_actual_foto.url, name_epic_picture_template)
 
 
-def get_url(url):
-    response = requests.get(url, params=input_payload())
+def get_url(url, api_key_foto):
+    response = requests.get(url, params=api_key_foto)
     response.raise_for_status()
     return response
-
-
-def input_payload():
-    epic_api_token = 'DEMO_KEY'
-    if input() != epic_api_token:
-        print("Введите токен:".replace("\r", ""))
-        return {"api_key": input()}
-    else:
-        return {"api_key": epic_api_token}
 
 
 def main():
