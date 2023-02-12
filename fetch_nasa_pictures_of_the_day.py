3875904b036b05cc29d1fd351fb00a58921ac02e
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-02-13 00:19:10 +0400

    optimized code

--- fetch_nasa_pictures_of_the_day.py
+++ fetch_nasa_pictures_of_the_day.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 def fetch_nasa_pictures_of_the_day(response):
 
     nasa_pictures_of_the_day = response.json()
-    name_picture_template = """nasa_apod{number}.jpg""".format(number=str(nasa_pictures_of_the_day['date']))
+    name_picture_template = "nasa_apod{number}.jpg".format(number=nasa_pictures_of_the_day['date'])
     picture_for_telegram = os.path.join("APOD", name_picture_template)
     download_images(nasa_pictures_of_the_day['url'], picture_for_telegram)
 
